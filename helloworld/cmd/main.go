130d2bc65e8bb04b2e5e9da55126f02f5e906681
kitchen <kitchen-ms@outlook.com>
2021-02-21 15:18:16 +0800

    first commit

---
+++ helloworld/cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"defaas/helloworld" // import helloworld
+
+	"github.com/FISCO-BCOS/go-sdk/client"
+	"github.com/FISCO-BCOS/go-sdk/conf"
+)
+
+func main() {
+	configs, err := conf.ParseConfigFile("config.toml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	config := &configs[0]
+
+	client, err := client.Dial(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	address, tx, instance, err := helloworld.DeployHelloWorld(client.GetTransactOpts(), client) // deploy contract
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("contract address: ", address.Hex()) // the address should be saved
+	fmt.Println("transaction hash: ", tx.Hash().Hex())
+	_ = instance
+}
